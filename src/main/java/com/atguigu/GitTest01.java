f6351aee6c5de0c32144fbd748aa2ed51cc8049d
zhanglin <jackyzhanglin@163.com>
2022-05-01 14:10:26 +0800

    first commit

--- src/main/java/com/atguigu/GitTest01.java
+++ src/main/java/com/atguigu/GitTest01.java
@@ -2,6 +2,7 @@ package com.atguigu;
 
 public class GitTest01 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
